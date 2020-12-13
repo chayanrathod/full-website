975e1762b4113dc36f0099c65872983c2f16fe97
chayanrathod <75927316+chayanrathod@users.noreply.github.com>
2020-12-13 16:30:17 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,49 +1,17 @@
 from django.db import models
-from django.db import OperationalError
 
-  
 # Create your models here.
-class product(models.Model):
-	product_id=models.AutoField
-	product_name=models.CharField(max_length=50)
-	category=models.CharField(max_length=50,default="")
-	subcategory=models.CharField(max_length=50,default="")
-	price=models.IntegerField(default=0)
-	desc=models.CharField(max_length=300,default="")
-	date=models.DateField()
-	image=models.ImageField(upload_to="filp/images",default='')
-	
-	def __str__(self):
-		return self.product_name
-	
-class contact(models.Model):
-    msg_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
-    email = models.CharField(max_length=70, default="")
-    phone = models.CharField(max_length=70, default="")
-    desc = models.CharField(max_length=500, default="")
-
+class Blogpost(models.Model):
+    post_id = models.AutoField(primary_key= True)
+    tilte = models.CharField(max_length=50)
+    head0 = models.CharField(max_length=500, default="")
+    chead0 = models.CharField(max_length=5000, default="")
+    head1 = models.CharField(max_length=500, default="")
+    chead1 = models.CharField(max_length=5000, default="")
+    head2 = models.CharField(max_length=500, default="")
+    chead2 = models.CharField(max_length=5000, default="")
+    pub_date = models.DateField()
+    thumbnail = models.ImageField(upload_to='filp/images', default="")
 
     def __str__(self):
-        return self.name
-    
-class Order(models.Model):
-    order_id = models.AutoField(primary_key=True)
-    items_json = models.CharField(max_length=5000)
-    amount=models.IntegerField(default=0)
-    name = models.CharField(max_length=90)
-    email = models.CharField(max_length=111)
-    address = models.CharField(max_length=111)
-    city = models.CharField(max_length=111)
-    state = models.CharField(max_length=111)
-    zip_code = models.CharField(max_length=111)
-    phone = models.CharField(max_length=111, default="")
-   
-class OrderUpdate(models.Model):
-    update_id  = models.AutoField(primary_key=True)
-    order_id = models.IntegerField(default="")
-    update_desc = models.CharField(max_length=5000,default='')
-    timestamp = models.DateField(auto_now_add=True)
-
-    def __str__(self):
-        return self.update_desc[0:7] + "..."
+        return self.tilte
