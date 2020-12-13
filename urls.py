ce0098fed66fe87c8233388e4df4c5e2674708f3
chayanrathod <75927316+chayanrathod@users.noreply.github.com>
2020-12-13 16:30:54 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,14 +1,8 @@
 from django.urls import path
 from . import views
+#from django.conf import settings
+#from django.conf.urls.static import static
 urlpatterns = [
     path('',views.index,name='index'),
-    path('about/',views.about,name='about'),
-    path('cont/',views.cont,name='cont'),
-    path('tracker/',views.tracker,name='tracker'),
-    path('productview/<int:myid>',views.productview,name='productview'),
-    path('addtocart/',views.addtocart,name='addtocart'),
-    path('checkout/',views.checkout,name='checkout'),
-    path('search/',views.search,name='search'),
-    #path("handlerequest/", views.handlerequest, name="HandleRequest"),
-
+    path('blog/<int:id>',views.blog,name='blog')
 ]
