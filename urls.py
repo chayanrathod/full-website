30e795f0f21958e747a0fc69bd360d04546cac1a
chayanrathod <75927316+chayanrathod@users.noreply.github.com>
2020-12-13 16:27:49 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,27 +1,14 @@
-"""ecom URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.db import OperationalError
-from django.urls import path,include
-from django.conf import settings
-from django.conf.urls.static import static
-
+from django.urls import path
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-	path('blog/',include('blog.urls')),
-	path('',include('filp.urls')),
-*  static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    path('',views.index,name='index'),
+    path('about/',views.about,name='about'),
+    path('cont/',views.cont,name='cont'),
+    path('tracker/',views.tracker,name='tracker'),
+    path('productview/<int:myid>',views.productview,name='productview'),
+    path('addtocart/',views.addtocart,name='addtocart'),
+    path('checkout/',views.checkout,name='checkout'),
+    path('search/',views.search,name='search'),
+    #path("handlerequest/", views.handlerequest, name="HandleRequest"),
+
 ]
